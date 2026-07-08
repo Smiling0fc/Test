4f42543eae5cdcf31bc7c5b416c734d9974f87d4
Smiling0fc <47120044+Smiling0fc@users.noreply.github.com>
2026-07-08 19:19:55 +0300

    Update app.js

--- AdminGroupSettings/js/app.js
+++ AdminGroupSettings/js/app.js
@@ -1,1 +1,31 @@
+class App {
 
+    static start() {
+
+        if (!Auth.isLogged()) {
+
+            Login.render();
+
+            return;
+
+        }
+
+        Layout.render();
+
+        Sidebar.render();
+
+        Header.render("Dashboard");
+
+        Dashboard.render();
+
+        Navigation.init();
+
+    }
+
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    App.start();
+
+});
