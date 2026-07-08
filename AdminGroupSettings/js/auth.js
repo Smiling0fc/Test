15329d863a4124d82cf9bcfae18872b5db33d7af
Smiling0fc <47120044+Smiling0fc@users.noreply.github.com>
2026-07-08 19:05:52 +0300

    Implement Auth class for login/logout functionality

--- AdminGroupSettings/js/auth.js
+++ AdminGroupSettings/js/auth.js
@@ -1,1 +1,33 @@
 
+class Auth {
+
+    static login(){
+
+        localStorage.setItem(
+            "logged",
+            "true"
+        );
+
+        App.start();
+
+    }
+
+    static logout(){
+
+        localStorage.removeItem(
+            "logged"
+        );
+
+        App.start();
+
+    }
+
+    static isLogged(){
+
+        return localStorage.getItem(
+            "logged"
+        ) === "true";
+
+    }
+
+}
