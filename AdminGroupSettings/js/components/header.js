04ce99b9bdf131da09c42fe07c1a11c915059125
Smiling0fc <47120044+Smiling0fc@users.noreply.github.com>
2026-07-09 10:03:48 +0300

    Implement Header class with static render method

--- AdminGroupSettings/js/components/header.js
+++ AdminGroupSettings/js/components/header.js
@@ -1,1 +1,41 @@
+class Header {
 
+    static render(title = "Dashboard"){
+
+        const header = document.getElementById("header");
+
+        header.innerHTML = `
+
+            <div class="header-title">
+
+                ${title}
+
+            </div>
+
+            <div class="header-user">
+
+                <span>Администратор</span>
+
+                <div class="avatar">
+                    A
+                </div>
+
+                <button
+                    id="logoutButton"
+                    class="logoutButton">
+
+                    Выйти
+
+                </button>
+
+            </div>
+
+        `;
+
+        document
+            .getElementById("logoutButton")
+            .onclick = Auth.logout;
+
+    }
+
+}
