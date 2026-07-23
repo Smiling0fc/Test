caa8b7d5c26689ef603ac800337eacd5270b6317
Smiling0fc <47120044+Smiling0fc@users.noreply.github.com>
2026-07-23 17:53:06 +0300

    Update CMS version to 0.3.2 and add apiUrl

--- AdminGroupSettings/js/config.js
+++ AdminGroupSettings/js/config.js
@@ -1,7 +1,9 @@
 const CMS_CONFIG = {
 
-    version: "0.3.1",
+    version: "0.3.2",
 
-    build: "2026-07-23"
+    build: "2026-07-23",
+
+    apiUrl: "https://script.google.com/macros/s/AKfycbxcXnldGSY-y85UAvaBYRcqoMxtKbnXu4AEpDlmNRVUaGyMhzVzBFOWr8x6oht0HycSBw/exec"
 
 };
