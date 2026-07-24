8f081199e91d862b7bdacb8b8bdccf5bce16c9b7
Smiling0fc <47120044+Smiling0fc@users.noreply.github.com>
2026-07-24 19:25:25 +0300

    Add public configuration for API and image sizes

---
+++ js/publicConfig.js
@@ -0,0 +1,15 @@
+const PUBLIC_CONFIG = {
+
+    apiUrl: "https://script.google.com/macros/s/AKfycbxcXnldGSY-y85UAvaBYRcqoMxtKbnXu4AEpDlmNRVUaGyMhzVzBFOWr8x6oht0HycSBw/exec",
+
+    imageSizes: {
+
+        card: 900,
+
+        preview: 1600,
+
+        original: 2400
+
+    }
+
+};
