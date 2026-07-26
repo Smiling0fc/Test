57531117afb93a53969651f2ae3ab91e37627123
Smiling0fc <47120044+Smiling0fc@users.noreply.github.com>
2026-07-26 03:49:36 +0300

    Update CMS version to v0.4.0 Editorial Design

--- AdminGroupSettings/js/config.js
+++ AdminGroupSettings/js/config.js
@@ -1,8 +1,10 @@
 const CMS_CONFIG = {
 
-    version: "0.3.2",
+    version: "0.4.0",
 
-    build: "2026-07-23",
+    codename: "Editorial Design",
+
+    build: "2026-07-26",
 
     apiUrl: "https://script.google.com/macros/s/AKfycbxcXnldGSY-y85UAvaBYRcqoMxtKbnXu4AEpDlmNRVUaGyMhzVzBFOWr8x6oht0HycSBw/exec"
 
