aaa6e6a53a71684442250b4fc0fbbb6d087a1456
Smiling0fc <47120044+Smiling0fc@users.noreply.github.com>
2026-07-31 10:52:23 +0300

    Update Studio version to v.0.5.1

--- AdminGroupSettings/js/config.js
+++ AdminGroupSettings/js/config.js
@@ -1,10 +1,10 @@
 const CMS_CONFIG = {
 
-    version: "0.4.0",
+    version: "0.5.1",
 
     codename: "Editorial Design",
 
-    build: "2026-07-26",
+    build: "2026-07-31",
 
     apiUrl: "https://script.google.com/macros/s/AKfycbxcXnldGSY-y85UAvaBYRcqoMxtKbnXu4AEpDlmNRVUaGyMhzVzBFOWr8x6oht0HycSBw/exec"
 
